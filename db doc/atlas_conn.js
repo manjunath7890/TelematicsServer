9f0e233534128bdd7ae633361b21e0ed77b71ab0
manjunath7890 <flyingfortress289@gmail.com>
2025-01-15 14:57:19 +0530

    atlas commit

--- db doc/atlas_conn.js
+++ db doc/atlas_conn.js
@@ -2,8 +2,8 @@
 
 const mongoose = require('mongoose');
 // const db = process.env.db;
-// const db = "mongodb+srv://flyingfortress289:flyingfortress289@cluster0.zlhd1zd.mongodb.net/?retryWrites=true&w=majority"; 
-const db = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.7";
+const db = "mongodb+srv://flyingfortress289:flyingfortress289@cluster0.zlhd1zd.mongodb.net/?retryWrites=true&w=majority"; 
+// const db = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.7";
 
 mongoose.connect(db, {
     useNewUrlParser: true,
